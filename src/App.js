05ae88dce5455ef3f602910350a61af168b026d8
Harinath8 <harinath8163@gmail.com>
2020-12-08 13:13:29 +0530

    Comit 1

--- src/App.js
+++ src/App.js
@@ -1,62 +1,106 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { Redirect, Route, Switch, withRouter } from 'react-router-dom';
-import ApproveOrUnapproveTimesheet from './containers/ApproveOrUnapproveTimesheet/ApproveOrUnapproveTimesheet';
-import Logout from './containers/Employee/Logout/Logout';
-import SignIn from './containers/Employee/SignIn/SignIn';
-import Timesheet from './containers/Timesheet/Timesheet';
-import Layout from './hoc/Layout/Layout';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import {
+  Redirect,
+  Route,
+  Switch,
+  useHistory,
+  withRouter,
+} from "react-router-dom";
 
-import * as actions from './store/actions/index';
+// import SignIn from "./containers/Employee/SignIn/SignIn";
+// import Timesheet from "./containers/Timesheet/Timesheet";
+// import ApproveOrUnapproveTimesheet from "./containers/ApproveOrUnapproveTimesheet/ApproveOrUnapproveTimesheet";
+// import Logout from "./containers/Employee/Logout/Logout";
+import Layout from "./hoc/Layout/Layout";
+
+import routes from './routes';
+import * as actions from "./store/actions/index";
+
+
+const RenderRoutes = (route) => {
+  const history = useHistory();
+  document.title = route.name;
+  if (route.needsAuth && this.props.isAuthenticated) {
+    history.push("/signin");
+  }
+
+  return (
+      <Route
+        path={route.path}
+        exact
+        // component={route.component}
+        render={(props) => <route.component {...props} />}
+      >
+      </Route>
+  );
+
+  // <Switch>
+  //   {routes.map((route) => {
+  //     document.title = route.name;
+  //     return (
+  //       <Route
+  //         path={route.path}
+  //         exact
+  //         component={route.component}
+  //         key={route.name}
+  //       />
+  //     );
+  //   })}
+  //   <Redirect to="/" />
+  // </Switch>;
+};
 
 class App extends Component {
-
   componentDidMount() {
     this.props.onTryAutoSignup();
   }
 
   render() {
-    let routes = (
-      <Switch>
-        <Route path="/signin">
-          <SignIn />
-        </Route>
-        <Route exact path="/" component={SignIn} />
-        <Redirect to="/" />
-      </Switch>
-    );
+    // let routes = (
+    //   <Switch>
+    //     <Route path="/signin">
+    //       <SignIn />
+    //     </Route>
+    //     <Route exact path="/" component={SignIn} />
+    //     <Redirect to="/" />
+    //   </Switch>
+    // );
 
-    if (this.props.isAuthenticated) {
-      routes = (
-        <Switch>
-          <Route path="/logout" component={Logout} />
-          <Route path="/approveorUnapprove" component={ApproveOrUnapproveTimesheet} />
-          <Route path="/employeeTimesheet" component={Timesheet} />
-          <Route exact path="/" component={Timesheet} />
-          <Redirect to="/" />
-        </Switch>
-      );
-    }
+    // if (this.props.isAuthenticated) {
+    //   routes = (
+    //     <Switch>
+    //       <Route path="/logout" component={Logout} />
+    //       <Route path="/approveorUnapprove" component={ApproveOrUnapproveTimesheet} />
+    //       <Route path="/employeeTimesheet" component={Timesheet} />
+    //       <Route exact path="/" component={Timesheet} />
+    //       <Redirect to="/" />
+    //     </Switch>
+    //   );
+    // }
 
     return (
       <Layout>
-        {routes}
+        <Switch>
+          {routes.map(route => (
+            <RenderRoutes {...route} key={route.name} />
+          ))}
+          <Redirect to="/" />
+        </Switch>
       </Layout>
-    );
-
+    )
   }
-
 }
 
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
   return {
-    isAuthenticated: state.token !== null && state.employeeId !== null
+    isAuthenticated: state.token !== null && state.employeeId !== null,
   };
 };
 
-const mapDispatchToProps = dispatch => {
+const mapDispatchToProps = (dispatch) => {
   return {
-    onTryAutoSignup: () => dispatch(actions.authCheckState())
+    onTryAutoSignup: () => dispatch(actions.authCheckState()),
   };
 };
 
